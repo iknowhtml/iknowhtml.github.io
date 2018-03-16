a9ddca612f29e2ee18cf4923290efe0f0264695d
Aki Gao <iknowsmtp@gmail.com>
2018-03-15 23:51:56 -0400

    Remove outer div element, update character delay prop value

--- src/App/App.js
+++ src/App/App.js
@@ -4,13 +4,11 @@ import style from './app.css';
 import Typing from './typing';
 
 const app = () => (
-  <div>
-    <Typing
-      text="Hi, I'm Aki. This is my website. Learn more about me below."
-      characterDelay={50}
-      punctuationDelay={250}
-    />
-  </div>
+  <Typing
+    text="Hi, I'm Aki. This is my website. Learn more about me below."
+    characterDelay={65}
+    punctuationDelay={250}
+  />
 );
 
 export default app;
