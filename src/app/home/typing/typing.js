5f494792b332e82f50bd7090236ae479350a6a73
Aki Gao <iknowsmtp@gmail.com>
2018-03-18 22:11:20 -0400

    Add Cursor to Typing component, add logic for displaying input

--- src/app/home/typing/typing.js
+++ src/app/home/typing/typing.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Cursor from './cursor';
+
 class Typing extends React.Component {
   state = {
     text: this.props.children,
     index: 0,
+    isFinished: false,
     characterDelay: this.props.characterDelay,
     punctuationDelay: this.props.punctuationDelay,
   };
@@ -13,8 +16,15 @@ class Typing extends React.Component {
   }
 
   componentDidUpdate() {
-    if (this.state.index === this.state.text.length) {
+    if (
+      this.state.index === this.state.text.length &&
+      this.state.isFinished === false
+    ) {
       clearInterval(this.intervalID);
+      setTimeout(() => {
+        this.setState({ isFinished: true });
+        this.props.setRenderInputTrue();
+      }, 700);
     } else if (this.checkPunctuation()) {
       clearInterval(this.intervalID);
       setTimeout(() => {
@@ -44,6 +54,7 @@ class Typing extends React.Component {
     return (
       <React.Fragment>
         {this.state.text.substring(0, this.state.index)}
+        {this.state.isFinished === false ? <Cursor /> : null}
       </React.Fragment>
     );
   }
