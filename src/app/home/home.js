97c306b8cc59d41bff3d462aaec5e71f5ec577b7
Aki Gao <iknowsmtp@gmail.com>
2018-03-18 22:11:59 -0400

    Add transition animations for displaying text input

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import Typing from './typing';
 
 import style from './home.css';
 
 class Home extends React.Component {
+  state = {
+    shouldRenderInput: false,
+  };
+
+  setRenderInputTrue = () => {
+    this.setState({ shouldRenderInput: true });
+    this.searchBar.focus();
+  };
+
   render() {
+    const searchBar = classNames({
+      [style.searchBar]: true,
+      [style.hide]: this.state.shouldRenderInput ? false : true,
+    });
+
     return (
       <div className={style.home}>
         <div className={style.mainHeader}>
-          <Typing characterDelay={50} punctuationDelay={500}>
+          <Typing
+            characterDelay={50}
+            punctuationDelay={500}
+            setRenderInputTrue={this.setRenderInputTrue}
+          >
             Hi, I'm Aki. This is my website. Learn more about me below.
           </Typing>
         </div>
         <input
-          className={style.searchBar}
+          className={searchBar}
           type="text"
           placeholder="e.g., about, resume, projects"
+          ref={input => {
+            this.searchBar = input;
+          }}
         />
       </div>
     );
