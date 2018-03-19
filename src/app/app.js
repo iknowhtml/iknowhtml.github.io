b80bc85868205770241ae71542f707b78447febc
Aki Gao <iknowsmtp@gmail.com>
2018-03-18 20:27:41 -0400

    Wrap Home container in container component

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import './Raleway-Light.ttf';
+
 import style from './app.css';
 
 import Home from './home';
 
-const app = () => <Home />;
+const app = () => (
+  <div className={style.container}>
+    <Home />
+  </div>
+);
 
 export default app;
