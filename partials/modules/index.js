ff0f64d0397753f515e36469169aafa0e8940285
Aki Gao <iknowsmtp@gmail.com>
2018-03-25 18:11:06 -0400

    Remove partials directory in favor of webpack submodule

--- partials/modules/index.js
+++
@@ -1,5 +0,0 @@
-import babel from './babel';
-import postCSS from './postCSS';
-import fonts from './fonts';
-
-export { babel, postCSS, fonts, };
--- partials/plugins/index.js
+++
@@ -1,4 +0,0 @@
-import htmlWebpack from './htmlWebpack';
-import hotModuleReplacement from './hotModuleReplacement';
-
-export { htmlWebpack, hotModuleReplacement, };
