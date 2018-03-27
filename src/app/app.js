2a3c9e828873099dbbf88a81528b9bba801c01db
Aki Gao <iknowsmtp@gmail.com>
2018-03-26 21:43:23 -0400

    Add basic autocomplete, refactor header functionality into typing

--- src/app/app.js
+++ src/app/app.js
@@ -10,40 +10,42 @@ import style from './app.css';
 class App extends React.Component {
   state = {
     headerText: "Hi, I'm Aki. this is my website. Learn more about me below.",
-    displayInput: false,
+    renderInput: false,
   };
 
   onInputChange = event => {
-    const value = event.target.value;
-    this.searchBar.setSelectionRange(0, value.length);
+    if (this.autoCompleteTimeout) {
+      clearTimeout(this.autoCompleteTimeout);
+    }
+    const startIndex = event.target.value.length;
+    this.autoCompleteTimeout = setTimeout(() => {
+      this.searchBar.value = 'about';
+      this.searchBar.setSelectionRange(startIndex, this.searchBar.value.length);
+    }, 300);
   };
 
-  setDisplayInputTrue = () => {
-    this.setState({ displayInput: true });
+  renderInput = () => {
+    this.setState({ renderInput: true });
     this.searchBar.focus();
   };
 
   render() {
     const searchBar = classNames({
       [style.searchBar]: true,
-      [style.hide]: this.state.displayInput ? false : true,
+      [style.hide]: this.state.renderInput ? false : true,
     });
 
     return (
       <div className={style.container}>
         <div className={style.header}>
-          {this.state.displayInput ? (
-            this.state.headerText
-          ) : (
-            <Typing
-              characterDelay={50}
-              punctuationDelay={500}
-              isFinished={this.state.displayInput}
-              setDisplayInputTrue={this.setDisplayInputTrue}
-            >
-              {this.state.headerText}
-            </Typing>
-          )}
+          <Typing
+            characterDelay={50}
+            punctuationDelay={500}
+            isFinished={this.state.renderInput}
+            onCompleteTyping={this.renderInput}
+          >
+            {this.state.headerText}
+          </Typing>
         </div>
         <input
           className={searchBar}
