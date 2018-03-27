1d44d365805a9d7a8922c145a08df0c90f9c7483
Aki Gao <iknowsmtp@gmail.com>
2018-03-27 15:33:25 -0400

    Update header, add header transitions

--- src/app/app.js
+++ src/app/app.js
@@ -9,19 +9,51 @@ import style from './app.css';
 
 class App extends React.Component {
   state = {
-    headerText: "Hi, I'm Aki. this is my website. Learn more about me below.",
+    headerTexts: [
+      "Hi, I'm Aki. this is my website. Learn more about me below.",
+      'Aki Gao - 1 search result',
+      'Aki Gao - 0 search result',
+    ],
+    headerTextsIndex: 0,
+    pages: ['about', 'resume', 'projects'],
     renderInput: false,
   };
 
-  onInputChange = event => {
+  onInputKeyUp = event => {
     if (this.autoCompleteTimeout) {
       clearTimeout(this.autoCompleteTimeout);
     }
-    const startIndex = event.target.value.length;
-    this.autoCompleteTimeout = setTimeout(() => {
-      this.searchBar.value = 'about';
-      this.searchBar.setSelectionRange(startIndex, this.searchBar.value.length);
-    }, 300);
+
+    let didAutoComplete = false;
+
+    const autoComplete = (value, startIndex) => {
+      this.autoCompleteTimeout = setTimeout(() => {
+        this.searchBar.value = value;
+        this.searchBar.setSelectionRange(startIndex, value.length);
+      }, 250);
+    };
+
+    this.state.pages.forEach(page => {
+      const regex = new RegExp(`^${event.target.value}`);
+      if (regex.test(page) && event.key !== 'Backspace') {
+        didAutoComplete = true;
+        autoComplete(page, event.target.value.length);
+      }
+    });
+
+    if (didAutoComplete) {
+      this.setState({
+        headerTextsIndex: 1,
+      });
+    } else if (event.target.value !== '') {
+      this.setState({
+        headerTextsIndex: 2,
+      });
+    } else {
+      this.setState({
+        headerTextsIndex: 0,
+      });
+    }
   };
 
   renderInput = () => {
@@ -38,20 +70,23 @@ class App extends React.Component {
     return (
       <div className={style.container}>
         <div className={style.header}>
-          <Typing
-            characterDelay={50}
-            punctuationDelay={500}
-            isFinished={this.state.renderInput}
-            onCompleteTyping={this.renderInput}
-          >
-            {this.state.headerText}
-          </Typing>
+          {this.state.renderInput ? (
+            this.state.headerTexts[this.state.headerTextsIndex]
+          ) : (
+            <Typing
+              characterDelay={50}
+              punctuationDelay={500}
+              onCompleteTyping={this.renderInput}
+            >
+              {this.state.headerTexts[0]}
+            </Typing>
+          )}
         </div>
         <input
           className={searchBar}
           type="text"
           placeholder="e.g., about, resume, projects"
-          onChange={this.onInputChange}
+          onKeyUp={this.onInputKeyUp}
           ref={input => {
             this.searchBar = input;
           }}
