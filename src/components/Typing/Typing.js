4cf9ab67b5368c2a53888a18dd524cb0f3911f38
iknowhtml <iknowsmtp@gmail.com>
2019-08-07 19:35:24 -0400

    Update component logic, remove styling import

--- src/components/Typing/Typing.js
+++ src/components/Typing/Typing.js
@@ -1,30 +1,33 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { PropTypes } from 'prop-types';
-import { cursor } from './cursor.postcss';
+import useIsTypingComplete from '../../utils/effects/useIsTypingComplete';
 
 const Typing = ({ characterDelay, punctuationDelay, children }) => {
   const [index, updateIndex] = useState(1);
+  const [isTypingComplete, updateIsTypingComplete] = useIsTypingComplete();
 
   const isPunctuation = character => /,|\.|;|!/.test(character);
-  const hasCompletedTyping = index < children.length;
 
   useEffect(() => {
-    if (hasCompletedTyping) {
+    if (index >= children.length) {
+      updateIsTypingComplete(true);
+    } else {
       setTimeout(
         () => updateIndex(index + 1),
+        // checks index - 1 to ensure longer delay after a punctuation has been "typed"
         isPunctuation(children.charAt(index - 1))
           ? punctuationDelay
           : characterDelay
       );
     }
-  });
+  }, [index]);
 
-  return (
+  return isTypingComplete ? (
+    children
+  ) : (
     <Fragment>
-      {[
-        children.substring(0, index),
-        hasCompletedTyping ? <span className={cursor} /> : null,
-      ]}
+      {children.substring(0, index)}
+      <span />
     </Fragment>
   );
 };
