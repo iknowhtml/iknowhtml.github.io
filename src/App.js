88c2fb815b026dd5612e2bb4834d1f7748460324
iknowhtml <iknowsmtp@gmail.com>
2019-08-06 21:50:40 -0400

    Add Typing component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typing from './components/Typing';
+
+const App = () => (
+  <Typing characterDelay={50} punctuationDelay={500}>
+    Hi, my name is Aki. Welcome to my website. Learn more about me below!
+  </Typing>
+);
+
+export default App;
