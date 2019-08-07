74de36683f0849406f9909cbea8c4073243c1548
iknowhtml <iknowsmtp@gmail.com>
2019-08-07 19:32:43 -0400

    Wrap App with StateProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { StateProvider } from './utils/state';
+import './global.postcss';
+import 'typeface-raleway';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <StateProvider>
+    <App />
+  </StateProvider>,
+  document.getElementById('root')
+);
