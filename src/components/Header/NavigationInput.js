d6d8fbdd8e35870c8916355625b7f5e6f1af9c16
iknowhtml <iknowsmtp@gmail.com>
2019-08-07 22:28:41 -0400

    Complete Navigation Input component implementation

--- src/components/Header/NavigationInput.js
+++ src/components/Header/NavigationInput.js
@@ -1,9 +1,17 @@
-import React, { useRef, useEffect } from 'react';
-import useIsTypingComplete from '../../utils/effects/useIsTypingComplete';
+import React, { useState, useRef, useEffect } from 'react';
 import { hidden } from '../../global.postcss';
+import useIsTypingComplete from '../../utils/effects/useIsTypingComplete';
+import usePath from '../../utils/effects/usePath';
+
+const pages = ['about', 'resume', 'projects'];
 
 const NavigationInput = () => {
+  const [autoCompleteTimeoutID, updateAutoCompleteTimeoutID] = useState(null);
+  const [autoCompleteStartIndex, updateAutoCompleteStartIndex] = useState(-1);
+
   const [isTypingComplete] = useIsTypingComplete();
+  const [path, updatePath] = usePath();
+
   const inputElement = useRef(null);
 
   useEffect(() => {
@@ -12,11 +20,44 @@ const NavigationInput = () => {
     }
   }, [isTypingComplete]);
 
+  useEffect(() => {
+    pages.forEach(page => {
+      if (path === page) {
+        inputElement.current.setSelectionRange(
+          autoCompleteStartIndex,
+          path.length
+        );
+      }
+    });
+  }, [path]);
+
+  const handleOnChange = event => {
+    updatePath(event.target.value);
+    clearTimeout(autoCompleteTimeoutID);
+    pages.forEach(page => {
+      const regExp = new RegExp(`^${event.target.value}`);
+      if (
+        regExp.test(page) &&
+        event.nativeEvent.inputType !== 'deleteContentBackward'
+      ) {
+        const startIndex = event.target.value.length;
+        updateAutoCompleteTimeoutID(
+          setTimeout(() => {
+            updateAutoCompleteStartIndex(startIndex);
+            updatePath(page);
+          }, 300)
+        );
+      }
+    });
+  };
+
   return (
     <input
       ref={inputElement}
       type="text"
-      placeholder="e.g., about, resume, project"
+      placeholder="e.g., about, resume, projects"
+      value={path}
+      onChange={handleOnChange}
       className={isTypingComplete ? null : hidden}
     />
   );
