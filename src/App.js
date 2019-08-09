f89ac43707b6f14de424df4dcfea5469f6860e00
iknowhtml <iknowsmtp@gmail.com>
2019-08-08 21:53:47 -0400

    Add logic to bind url route with site navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Router, navigate } from '@reach/router';
 import Navigation from './components/Navigation';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+import usePath from './utils/effects/usePath';
 
 const App = () => {
-  return <Navigation></Navigation>;
+  const [path, updatePath] = usePath();
+
+  if (window.location.pathname === '/') {
+    if (window.location.search !== '') {
+      updatePath(window.location.search.substring(1));
+    }
+  }
+
+  useEffect(() => {
+    navigate(`/${path}`);
+  }, [path]);
+
+  return (
+    <Fragment>
+      <Header />
+      <Navigation />
+      <Router>
+        <About path="/about" />
+        <Resume path="/resume" />
+        <Projects path="/projects" />
+      </Router>
+    </Fragment>
+  );
 };
 
 export default App;
