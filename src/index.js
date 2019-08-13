ed9488df920704f141cc9718b3aabc3aaf96c016
iknowhtml <iknowsmtp@gmail.com>
2019-08-12 21:53:33 -0400

    Remove Raleway package, add Raleway locally

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { StateProvider } from './utils/state';
 import './global.postcss';
-import 'typeface-raleway';
 
 ReactDOM.render(
   <StateProvider>
