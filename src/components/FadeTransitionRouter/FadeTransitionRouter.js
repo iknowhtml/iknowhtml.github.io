e8172f00337e99044bf6923f09a969bae0a716ff
iknowhtml <iknowsmtp@gmail.com>
2019-10-21 19:31:42 -0400

    Improve transition of pages, add resume transition

--- src/components/FadeTransitionRouter/FadeTransitionRouter.js
+++ src/components/FadeTransitionRouter/FadeTransitionRouter.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { Router, Location } from '@reach/router';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import { PropTypes } from 'prop-types';
 import {
   enter,
   enterActive,
+  enterActiveResume,
   exit,
   exitActive,
 } from './FadeTransitionRouter.css';
+import usePath from '../../utils/effects/usePath';
 
-const FadeTransitionRouter = ({ children }) => (
-  <Location>
-    {({ location }) => (
-      <TransitionGroup component={null}>
-        <CSSTransition
-          key={location.key}
-          timeout={500}
-          classNames={{ enter, enterActive, exit, exitActive }}
-        >
-          <Router location={location}>{children}</Router>
-        </CSSTransition>
-      </TransitionGroup>
-    )}
-  </Location>
-);
+const FadeTransitionRouter = ({ children }) => {
+  const [path] = usePath();
+  return (
+    <Location>
+      {({ location }) => (
+        <SwitchTransition component={null}>
+          <CSSTransition
+            key={location.key}
+            timeout={{ enter: path === 'resume' ? 750 : 1250, exit: 1000 }}
+            classNames={{
+              enter,
+              enterActive: path === 'resume' ? enterActiveResume : enterActive,
+              exit,
+              exitActive,
+            }}
+          >
+            <Router location={location}>{children}</Router>
+          </CSSTransition>
+        </SwitchTransition>
+      )}
+    </Location>
+  );
+};
 
 FadeTransitionRouter.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
