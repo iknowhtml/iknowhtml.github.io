6a2771a7ee784dd2747b1ed3e0051364bd597ad2
iknowhtml <iknowsmtp@gmail.com>
2019-10-22 21:16:01 -0400

    Change stylesheet extension, add PDF download link

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import './Resume.css';
+import './Resume.postcss';
 
-const Projects = () => {
-  return <iframe title="resume" src="https://iknowht.ml/resume"></iframe>;
-};
+const Projects = () => (
+  <Fragment>
+    <iframe title="resume" src="https://iknowht.ml/resume"></iframe>
+    <h3>
+      <a href="https://iknowht.ml/resume/Aki%20Gao's%20Resume.pdf" download>
+        Download
+      </a>
+    </h3>
+  </Fragment>
+);
 
 export default Projects;
