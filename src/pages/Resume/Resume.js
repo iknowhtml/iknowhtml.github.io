d75541f7750f81c268de273f72346bf54fd9f867
iknowhtml <iknowsmtp@gmail.com>
2020-01-26 07:18:43 -0500

    Refactor download container from h3 to div element

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -5,11 +5,11 @@ import './Resume.postcss';
 const Projects = () => (
   <main>
     <iframe title="resume" src="https://iknowht.ml/resume"></iframe>
-    <h3>
+    <div>
       <a href="https://iknowht.ml/resume/Aki%20Gao's%20Resume.pdf" download>
         Download
       </a>
-    </h3>
+    </div>
   </main>
 );
 
