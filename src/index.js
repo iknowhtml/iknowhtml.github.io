20a73eccbd51bf9035f581e1e81545bfcffc48d5
iknowhtml <iknowsmtp@gmail.com>
2020-01-26 06:55:27 -0500

    Use raleway package instead of local font files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-raleway';
 import App from './App';
 import { StateProvider } from './utils/state';
 import './global.postcss';
