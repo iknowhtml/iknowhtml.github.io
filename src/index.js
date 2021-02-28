f17e0fe5fdbc7ec12a85da5916c3830808bd85cc
iknowhtml <iknowsmtp@gmail.com>
2021-02-28 10:16:39 -0500

    Add additional styling & micro-interactions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.postcss';
+
+window.addEventListener('mousemove', ({ x, y }) => {
+  const getSkew = (position, length) => 5 * (position / length - 0.5);
+  document.body.style.transform = `perspective(${
+    document.body.clientHeight
+  }px) rotateX(${getSkew(x, window.innerWidth)}deg) rotateY(${getSkew(
+    y,
+    window.innerHeight
+  )}deg)`;
+});
